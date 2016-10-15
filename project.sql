db5ff8dc74e29e7753915a3c7045f7e1d9dfb59c
sngg12 <sngg12@naver.com>
2016-10-16 03:51:39 +0900

    Add files via upload

--- project.sql
+++ project.sql
@@ -1,167 +1,170 @@
--- 수험생 person : 장애 여부, rpost1~raddr2 : 실제 거주지  
-create table examinee 
-( id varchar2(20) constraint examinee_id_pk primary key,
-  birth date not null,
-  nation varchar2(6) constraint examinee_nation_ck check(nation in('내국인', '외국인')),
-  sex varchar2(4) constraint examinee_sex_ck check(sex in('남자', '여자')),
-  name varchar2(20) not null,
-  passwd varchar2(20) not null,
-  pw_question varchar2(100) not null,
-  pw_answer varchar2(100) not null,
-  person varchar2(6) constraint taker_person_ck check(person in('예', '아니요')),
-  email varchar2(30) not null,
-  post1 varchar2(3) not null,
-  post2 varchar2(3) not null,
-  addr1 varchar2(100) not null,
-  addr2 varchar2(100) not null,
-  rpost1 varchar2(3) not null,
-  rpost2 varchar2(3) not null,
-  raddr1 varchar2(100) not null,
-  raddr2 varchar2(100) not null );
-
-  Drop table Place  purge;
-  create table Place
-( Megalopolis varchar2(200) not null,
-  City varchar2(200) not null,
-  Place varchar2(200) not null,
-  Room varchar2(100) not null,
-  PlaceNo number(5) primary key);
-  
-  Drop table Supervisor  purge;
-  create table Supervisor
-( SupervisorNo number(20) primary key,
-  SupervisorName varchar2(10) not null,
-  PlaceNo number(5) REFERENCES Place(PlaceNo));
-  
-  Drop table Board  purge;
-  create table Board
-( BoardNo number(20) primary key,
-  BoardTitle varchar2(100) not null,
-  BoardContent varchar2(1000) not null,
-  SupervisorNo number(20) REFERENCES Supervisor(SupervisorNo),
-  SubjectNo number(5) REFERENCES TestInfo(SubjectNo));
-  
-  Drop table TestInfo purge;
-  create table TestInfo
-( SubjectNo number(5) Primary key,
-  Subject varchar2(30) not null,
-  TestDate date not null);
-  
-  create SEQUENCE seq_SlateNo;
-  create SEQUENCE seq_PlaceNo;
-  create SEQUENCE seq_SupervisorNo;
-  create SEQUENCE seq_BoardNo;
-  create SEQUENCE seq_SubjectNo;
-  
   select *
   from tab;
-  
-  insert into EXAMINEE
-  VALUES ('asdf', '1992-01-01', '내국인', '남자', '킴철쑤', '1234', '비번질문, 답은 a',
-           'a', '예', 'asdf@naver.com', '335', '448', '서울 특별시 영등포구 여의도동', '22번지', '335', '448', '서울 특별시 영등포구 여의도동', '22번지');
-  commit;
-  select
-		id,birth, nation, sex, name, passwd, pw_question, pw_answer, person, email,
-		post1, post2, addr1, addr2, rpost1, rpost2, raddr1, raddr2
-		from examinee
-		where id = 'asdf' and passwd='1234';
-    
-/*새 테이블 새 테이블 새 테이블 새 테이블 새 테이블 새 테이블 새 테이블 새 테이블 새 테이블 새 테이블 새 테이블 새 테이블 새 테이블*/
 
-drop table EXAMINEE purge;
+/* 종목 종목 종목 종목 종목 종목 종목 종목 종목 종목 종목 종목 종목 종목 종목 종목 종목 */
+select *
+from Qualification;
+/* 종목 종목 종목 종목 종목 종목 종목 종목 종목 종목 종목 종목 종목 종목 종목 종목 종목 */
+
+/* 수험생 수험생 수험생 수험생 수험생 수험생 수험생 수험생 수험생 수험생 수험생 수험생 */
+select *
+from EXAMINEE;
+
+Drop table EXAMINEE  purge;
 
 create table EXAMINEE
-( eId VARCHAR2(20) CONSTRAINT EXAMINEE_eId_pk PRIMARY KEY,
-  ePw VARCHAR2(20) NOT NULL,
-  eName VARCHAR2(20) NOT NULL,
-  eEmail VARCHAR2(40) NOT NULL,
-  ePhone VARCHAR2(15) NOT NULL,
-  ePost VARCHAR2(6) NOT NULL,
-  eAddr1 VARCHAR2(100) NOT NULL,
-  eAddr2 VARCHAR2(100) NOT NULL,
-  ePath VARCHAR2(40) NOT NULL);
-  
-  
-  
-drop table SUPERVISOR purge;
+  (eId varchar2(20) primary key,
+  ePw varchar2(20) not null,
+  eName varchar2(20) not null,
+  eBirth date not null,
+  eEmail varchar2(40) not null,
+  ePhone varchar2(15) not null,
+  ePost varchar2(8) not null,
+  eAddr1 varchar2(100) not null,
+  eAddr2 varchar2(100) not null,
+  ePath varchar2(100) not null);
+/* 수험생 수험생 수험생 수험생 수험생 수험생 수험생 수험생 수험생 수험생 수험생 수험생 */
 
-create table SUPERVISOR
-( sId VARCHAR2(20) CONSTRAINT SUPERVISOR_sId_pk PRIMARY KEY,
-  sPw VARCHAR2(20) NOT NULL,
-  sName VARCHAR2(20) NOT NULL,
-  sEmail VARCHAR2(40) NOT NULL,
-  sPhone VARCHAR2(15) NOT NULL,
-  sPost VARCHAR2(6) NOT NULL,
-  sAddr1 VARCHAR2(100) NOT NULL,
-  sAddr2 VARCHAR2(100) NOT NULL,
-  sPath VARCHAR2(40) NOT NULL);
+/* 수험표 수험표 수험표 수험표 수험표 수험표 수험표 수험표 수험표 수험표 수험표 수험표 */
+  select *
+  from TEST_IDENTIFICATION_SLIP;
   
+  desc TEST_IDENTIFICATION_SLIP;
   
-  
-drop table LICENSE purge;
+  Drop table TEST_IDENTIFICATION_SLIP  purge;
+  create table TEST_IDENTIFICATION_SLIP
+  (eNo varchar2(14) primary key,
+  qCode number(4) references Qualification(qCode) not null,
+  eId varchar2(20)references EXAMINEE(eId) not null);
+/* 수험표 수험표 수험표 수험표 수험표 수험표 수험표 수험표 수험표 수험표 수험표 수험표 */
 
-create table LICENSE
-( eId VARCHAR2(20),
-  qCode NUMBER,*******
-  lDate DATE NOT NULL);
-  
-  
-  
-drop table QUALIFICATION purge;
+/* 감독관 감독관 감독관 감독관 감독관 감독관 감독관 감독관 감독관 감독관 감독관 감독관 */
+create table Supervisor
+  (ssId varchar2(20) primary key,
+  sPw varchar2(20) not null,
+  sName varchar2(20) not null,
+  sGroup varchar2(100) not null,
+  sPhone varchar2(15) not null,
+  sPost varchar2(8) not null,
+  sAddr1 varchar2(100) not null,
+  sAddr2 varchar2(100) not null,
+  sBirth date not null,
+  sPath varchar2(100) not null);
 
-create table QUALIFICATION
-( qCode NUMBER,*******
-  qName VARCHAR2(40) NOT NULL,
-  qFiled VARCHAR2(20) NOT NULL);
-  
-  
-  
-drop table SUBJECT purge;
+  insert into Supervisor (ssId, sPw, sName, sGroup, sPhone, sPost, sAddr1, sAddr2, sBirth, sPath)
+  values ('20011', '1234', '홍다희', '공주대학교 컴퓨터공학부', '010-9877-3688', '36554', '서울시 강남구 논현2동', '공주아파트', '82/05/02', '123');
+  commit;
+  update Supervisor
+  set spost='34568'
+  where ssid='20013';
+/* 감독관 감독관 감독관 감독관 감독관 감독관 감독관 감독관 감독관 감독관 감독관 감독관 */
 
-create table SUBJECT
-( qCode NUMBER,*******
-  sCategory VARCHAR2(4) CONSTRAINT SUBJECT_sCategory_ck CHECK(sCategory in('필기', '실기')),
-  subject1 VARCHAR2(40),
-  subject2 VARCHAR2(40),
-  subject3 VARCHAR2(40),
-  subject4 VARCHAR2(40),
-  subject5 VARCHAR2(40),
-  subject6 VARCHAR2(40));
-  
-  
-  
-drop table TEST_PLACE purge;
+/* 관리자 관리자 관리자 관리자 관리자 관리자 관리자 관리자 관리자 관리자 관리자 관리자 */
+select *
+from EXAM_MANAGER;
 
-create table TEST_PLACE
-( tpNo NUMBER(5) CONSTRAINT TEST_PLACE_tpNo_pk PRIMARY KEY,
-  tpTitle VARCHAR2(100) NOT NULL,
-  pNo NUMBER(5) CONSTRAINT TEST_PLACE_pNo_fk REFERENCES PLACE(pNo) NOT NULL,
-  tpRoom VARCHAR2(100) NOT NULL,
-  tpDate DATE NOT NULL,
-  Admission_Time VARCHAR2(20) NOT NULL,
-  SUPERVISOR1 VARCHAR2(40) CONSTRAINT TEST_PLACE_SUPERVISOR1_fk REFERENCES SUPERVISOR(sId),
-  SUPERVISOR2 VARCHAR2(40) CONSTRAINT TEST_PLACE_SUPERVISOR2_fk REFERENCES SUPERVISOR(sId));
-  
-  
-  
-drop table PLACE purge;
+  Drop table region  purge;
+  create table EXAM_MANAGER
+  (mId varchar2(20) primary key,
+   mPw varchar2(20) NOT NULL);
+   
+   insert into EXAM_MANAGER (mId, mPw)
+   values ('1234', '1234');
+/* 관리자 관리자 관리자 관리자 관리자 관리자 관리자 관리자 관리자 관리자 관리자 관리자 */
 
-create table PLACE
-( pNo NUMBER(5) CONSTRAINT TEST_PLACE_pNo_pk PRIMARY KEY,
-  pRegion VARCHAR2(100) NOT NULL,
-  pCity VARCHAR2(100) NOT NULL,
-  pPlace VARCHAR2(100) NOT NULL);
-  
-  
-  
-drop table TEST_IDENTIFICATION_SLIP purge;
+/* 필기합격여부 필기합격여부 필기합격여부 필기합격여부 필기합격여부 필기합격여부 필기합격여부 */
+Drop table Written_pass  purge;
 
-create table TEST_IDENTIFICATION_SLIP
-( eNumber NUMBER(8) CONSTRAINT TEST_IDENTIFICATION_SLIP_eNumber_pk PRIMARY KEY,
-  Title VARCHAR2(50) NOT NULL,
-  qCode NUMBER CONSTRAINT TEST_IDENTIFICATION_SLIP_qCode_fk REFERENCES QUALIFICATION(qCode) NOT NULL,
-  Type VARCHAR2(1) CONSTRAINT TEST_IDENTIFICATION_SLIP_Type_ck CHECK(Type in('필기', '실기')),
-  Category VARCHAR2(4) CONSTRAINT TEST_IDENTIFICATION_SLIP_Category_ck CHECK(sCategory in('필기', '실기')),
-  tpNo NUMBER(5) CONSTRAINT TEST_IDENTIFICATION_SLIP_tpNo_fk REFERENCES TEST_PLACE(tpNo) NOT NULL,
-  Seat VARCHAR2(20) NOT NULL,
-  Attendance VARCHAR2(3) CONSTRAINT TEST_IDENTIFICATION_SLIP_Attendance_ck CHECK(Type in('YES', 'NO')));
+create table Written_pass
+( eId VARCHAR2(20) references EXAMINEE(eId) NOT NULL,
+  qCode NUMBER(4) references Qualification(qCode) NOT NULL,
+  wpDate DATE NOT NULL);
+/* 필기합격여부 필기합격여부 필기합격여부 필기합격여부 필기합격여부 필기합격여부 필기합격여부 */
+
+/* 필기과목 필기과목 필기과목 필기과목 필기과목 필기과목 필기과목 필기과목 필기과목 필기과목 */
+create table Write_Subject
+( qCode NUMBER(4) references Qualification(qCode) NOT NULL,
+  wsName VARCHAR2(40) NOT NULL);
+  
+delete Write_Subject
+where qcode = '1900';
+
+insert into Write_Subject
+values (1511, '포장시험 및 평가');
+
+select *
+from Write_Subject
+order by 1;
+
+select a.WSNAME
+from (select wsname from WRITE_SUBJECT where qcode = '1320') a, (select wsname from WRITE_SUBJECT where qcode = '1322') b
+where a.WSNAME = b.WSNAME;
+/* 필기과목 필기과목 필기과목 필기과목 필기과목 필기과목 필기과목 필기과목 필기과목 필기과목 */
+
+/* 시험일정 시험일정 시험일정 시험일정 시험일정 시험일정 시험일정 시험일정 시험일정 시험일정 */
+select *
+from Test_Schedule; 
+
+create table Test_Schedule
+( tsNo number(4) primary key,
+  tsTitle VARCHAR2(40) NOT NULL,
+  tpDate DATE NOT NULL);
+
+insert into Test_Schedule
+values (1603, '3차 정기기사', '16/08/21');
+
+delete Test_Schedule
+where TSNO = 1603;
+/* 시험일정 시험일정 시험일정 시험일정 시험일정 시험일정 시험일정 시험일정 시험일정 시험일정 */
+
+/* 일정_감독관 일정_감독관 일정_감독관 일정_감독관 일정_감독관 일정_감독관 일정_감독관 일정_감독관 */
+drop table SCHEDULE_SUPERVISOR purge;
+
+create table SCHEDULE_SUPERVISOR
+( tsNo NUMBER(4) references TEST_SCHEDULE(tsNo) NOT NULL,
+  ssId  VARCHAR2(20) references SUPERVISOR(ssId) NOT NULL,
+  S_CODE VARCHAR2(8) references SCHOOL(S_CODE) NOT NULL
+);
+
+select *
+from SCHEDULE_SUPERVISOR
+order by s_code;
+/* 일정_감독관 일정_감독관 일정_감독관 일정_감독관 일정_감독관 일정_감독관 일정_감독관 일정_감독관 */
+
+/* 인적사항 인적사항 인적사항 인적사항 인적사항 인적사항 인적사항 인적사항 인적사항 인적사항 인적사항 */
+drop table PERSONAL_DATA purge;
+
+create table PERSONAL_DATA
+( ppNo VARCHAR2(14) references TEST_IDENTIFICATION_SLIP(eNo) primary key,
+  pType VARCHAR2(1) NOT NULL,
+  pCategory VARCHAR2(4) NOT NULL,
+  pSeat VARCHAR2(2) NOT NULL,
+  pAttendance VARCHAR2(1) NOT NULL,
+  pNote VARCHAR2(200),
+  CHECK (pType = 'A' or pType = 'B' and pCategory = '필기' or pCategory = '실기' and pAttendance = 'Y' or pAttendance = 'N')
+  );
+  
+select *
+from PERSONAL_DATA;
+  
+insert into personal_data
+values ('5000301001','A','필기','3','N','23');
+/* 인적사항 인적사항 인적사항 인적사항 인적사항 인적사항 인적사항 인적사항 인적사항 인적사항 인적사항 */
+
+
+/* 정오표 정오표 정오표 정오표 정오표 정오표 정오표 정오표 정오표 정오표 정오표 정오표 정오표 정오표 */
+drop table ERRATA_BOARD purge;
+
+create table ERRATA_BOARD
+( bNo NUMBER(10) primary key,
+  ssId VARCHAR2(20) references SUPERVISOR(ssId) NOT NULL,
+  bContent VARCHAR2(1000) NOT NULL,
+  bDate DATE NOT NULL
+);
+
+select *
+from ERRATA_BOARD;
+
+insert into ERRATA_BOARD
+values( 0, '20011', '이 글은 정오표 시험용 글입니다', sysdate);
+/* 정오표 정오표 정오표 정오표 정오표 정오표 정오표 정오표 정오표 정오표 정오표 정오표 정오표 정오표 */
